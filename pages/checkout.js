231928d1c98867bca08814fae79ecfbb16bd1040
Xelem <anselmchizurum@gmail.com>
2022-08-07 22:52:33 +0100

    Designed the success page

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,7 +1,15 @@
 const Checkout = () => {
   return (
-    <div className="container">
-      <h2>success</h2>
+    <div className="container py-4 bg-light">
+      <div className="p-3">
+        <div className=" row justify-content-center">
+          <div className="col-md-12 bg-white p-5 align-items-center my-5">
+            <p className="display-1 text-success fw-bold text-center my-5 py-5">
+              Success!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
